be7be7c972bc354cac0b8d5870476c57075c205d
Gavin Kasdorf <me@gkasdorf.com>
2023-05-14 21:47:27 -0800

    shabang

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#! /usr/bin/env node
+
 const {upload} = require("./src/upload");
 const {saveSettings, showSettings} = require("./src/settings");
 const args = require("minimist")(process.argv.slice(2));
